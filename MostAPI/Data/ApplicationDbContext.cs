8d0752b83443968846249d4e3459f2e6edaa5040
downhiill <downh068@gmail.com>
2024-12-25 23:44:46 +0200

    register Db

--- MostAPI/Data/ApplicationDbContext.cs
+++ MostAPI/Data/ApplicationDbContext.cs
@@ -6,6 +6,11 @@ namespace MostAPI.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Admin> admins { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Admin>().Property(a => a.ChatId).HasColumnName("chatid");
+        }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
