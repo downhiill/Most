bc1e49b1027118fe4a5cbd71989d1cebf6b7fdf0
downhiill <downh068@gmail.com>
2024-12-25 15:21:58 +0200

    Added CORS configuration

--- MostAPI/Program.cs
+++ MostAPI/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Добавляем настройку CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()    // Разрешаем доступ с любого источника
+              .AllowAnyMethod()    // Разрешаем любые методы HTTP (GET, POST и т.д.)
+              .AllowAnyHeader();   // Разрешаем любые заголовки
+    });
+});
+
+// Настройка Swagger (если она уже есть)
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,8 +27,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Включаем CORS для всех маршрутов
+app.UseCors("AllowAll");
 
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
