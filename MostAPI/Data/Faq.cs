40a2c1dcebc1907cf89ffd9a08c8975198480561
downhiill <downh068@gmail.com>
2024-12-31 13:25:57 +0200

    поменял тип string на int у Id

--- MostAPI/Data/Faq.cs
+++ MostAPI/Data/Faq.cs
@@ -5,7 +5,7 @@ namespace MostAPI.Data
 {
     public class Faq
     {
-        public string Id { get; set; } // ID записи
+        public int Id { get; set; } // ID записи
         public string Question { get; set; } // Вопрос
         public string Answer { get; set; } // Ответ
     }
