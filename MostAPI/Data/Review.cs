51013019ee5b730b25187cfc91a7b12e95ac0ada
downhiill <downh068@gmail.com>
2025-01-04 16:05:17 +0200

    112

--- MostAPI/Data/Review.cs
+++ MostAPI/Data/Review.cs
@@ -4,7 +4,7 @@ namespace MostAPI.Data
 {
     public class Review
     {
-        public int Id { get; set; }
+        public ObjectId Id { get; set; }
         public string FirstName { get; set; } 
         public string LastName { get; set; } 
         public string Username { get; set; } 
