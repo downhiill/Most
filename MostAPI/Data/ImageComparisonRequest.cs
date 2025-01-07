29798ebf500022f15c867f2bfe33ebbe70653bfb
downhiill <downh068@gmail.com>
2025-01-07 14:56:34 +0200

    йцукен

--- MostAPI/Data/ImageComparisonRequest.cs
+++ MostAPI/Data/ImageComparisonRequest.cs
@@ -4,7 +4,10 @@ namespace MostAPI.Data
 {
     public class ImageComparisonRequest
     {
-        public IFormFile? Image1 { get; set; } // Первое изображение (может быть null)
-        public IFormFile? Image2 { get; set; } // Второе изображение (может быть null)
+        [Required(ErrorMessage = "Первое изображение обязательно")]
+        public IFormFile Image1 { get; set; } // Первое изображение
+
+        [Required(ErrorMessage = "Второе изображение обязательно.")]
+        public IFormFile Image2 { get; set; } // Второе изображение
     }
 }
